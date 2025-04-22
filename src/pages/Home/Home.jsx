e4542c09869eb29d891741fc3cd3a84d84603eaf
arjahid <jhjahidhasanar@gmail.com>
2025-04-22 22:35:31 +0600

    added navbar and banner

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from './Banner/Banner';
 
 const Home = () => {
     return (
         <div>
-            Hi i am from home
+           <Banner></Banner>
         </div>
     );
 };
