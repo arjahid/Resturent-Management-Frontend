18e5614ef65420ca9377359b336e42f15cf5348d
arjahid <jhjahidhasanar@gmail.com>
2025-04-22 22:00:34 +0600

    added footer

--- src/layouts/MainLayouts.jsx
+++ src/layouts/MainLayouts.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from '../shared/Footer/Footer';
+import NavBar from '../shared/NavBar/NavBar';
 
 const MainLayouts = () => {
     return (
         <div>
+            <NavBar></NavBar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,8 @@ import { router } from './router/Router.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-     <RouterProvider router={router}/>
+    <div className='max-w-screen-xl mx-auto'>
+    <RouterProvider router={router}/>
+    </div>
   </StrictMode>,
 )
