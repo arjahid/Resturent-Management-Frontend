3642999ccb75c250bb492185fd4bb9b0282f6846
arjahid <jhjahidhasanar@gmail.com>
2025-04-23 14:57:04 +0600

    added order online section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from './Banner/Banner';
 import Category from './Category/Category';
+import Sort from './sorttext/Sort';
 
 const Home = () => {
     return (
         <div>
            <Banner></Banner>
+           <Sort></Sort>
            <Category></Category>
         </div>
     );
---
+++ src/pages/Home/sorttext/Sort.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Sort = () => {
+    return (
+        <div className="text-center p-6 bg-pink-50 rounded-lg shadow-md mb-3">
+            <h2 className="text-pink-600 font-bold text-lg">--From 11.00am to 10.00pm</h2>
+            <div className="my-4 mx-auto w-1/2 border-b-2 border-pink-600"></div>
+            <h2 className="text-pink-800 font-bold text-3xl">Order Online</h2>
+            <div className="my-4 mx-auto w-1/2 border-b-2 border-pink-800"></div>
+        </div>
+    );
+};
+
+export default Sort;
