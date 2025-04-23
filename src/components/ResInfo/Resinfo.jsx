2908f3f151edbff48a87f616833859788ad4a9e2
arjahid <jhjahidhasanar@gmail.com>
2025-04-23 23:35:25 +0600

    fix responsive

--- src/components/ResInfo/Resinfo.jsx
+++ src/components/ResInfo/Resinfo.jsx
@@ -17,7 +17,7 @@ const Resinfo = () => {
       <div className="hero-content justify-start items-start text-left px-4 sm:px-6 md:px-8 lg:px-12 pt-6 sm:pt-8 md:pt-10">
         <div className="max-w-lg bg-white bg-opacity-10 p-4 sm:p-6 md:p-8 rounded-lg border border-gray-300 shadow-lg">
           <h1 className="mb-4 sm:mb-6 text-4xl sm:text-5xl md:text-6xl font-extrabold text-white">
-            The Forest Restaurant
+            The <span className='text-green-500'>Forest</span> Restaurant
           </h1>
           <p className="mb-6 sm:mb-8 text-base sm:text-lg md:text-xl text-gray-300">
             Experience the finest dining in a serene forest ambiance. Enjoy our exquisite menu crafted with care and passion.
