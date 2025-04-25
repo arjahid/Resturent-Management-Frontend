a202a219319a4ddab9abbb6ded3ecf43210e61d0
arjahid <jhjahidhasanar@gmail.com>
2025-04-25 16:03:10 +0600

    added Our menu section necessary details and add paralax here

--- src/pages/Home/Menu/Menu.jsx
+++ src/pages/Home/Menu/Menu.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
-import { Helmet } from 'react-helmet-async';
-
-
+import React from "react";
+import { Helmet } from "react-helmet-async";
+import Cover from "../../../shared/Cover/Cover";
+import menu from '../../../assets/menu/banner3.jpg'
+import PropularMenu from "../PropularMenu/PropularMenu";
 
 const Menu = () => {
-    console.log('menu page render')
-    return (
-        <div>
-           <Helmet>
-                <title>Forest | Menu</title>
-               
-           </Helmet>
-            this is menu pagesssss
-        </div>
-    );
+  console.log("menu page render");
+  return (
+    <div>
+      <Helmet>
+        <title>Forest | Menu</title>
+      </Helmet>
+      <Cover img={menu} tittle="Our Menu"></Cover>
+      <PropularMenu></PropularMenu>
+      <Cover img={menu} tittle="Our Menu"></Cover>
+      <PropularMenu></PropularMenu>
+      <Cover img={menu} tittle="Our Menu"></Cover>
+      <PropularMenu></PropularMenu>
+      
+    
+    </div>
+  );
 };
 
 export default Menu;
