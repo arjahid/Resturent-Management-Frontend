9cb82f191763b7dce359ef0f0f577e5fc1d9994f
arjahid <jhjahidhasanar@gmail.com>
2025-04-27 23:44:16 +0600

    add from for login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
 
 const Login = () => {
+    const hanndleLogin =(e)=>{
+        
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value
+        console.log(email, password);
+    }
     return (
-        <div>
-            <h2>please login</h2>
+        <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Login now!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+              quasi. In deleniti eaque aut repudiandae et a id nisi.
+            </p>
+          </div>
+          <div className="card bg-base-100 w-full max-w-sm  shadow-2xl">
+            <div className="card-body">
+              <form onSubmit={hanndleLogin} className="fieldset">
+                <label className="label">Email</label>
+                <input type="email" name='email' className="input" placeholder="Email" />
+                <label className="label">Password</label>
+                <input type="password" name='password' className="input" placeholder="Password" />
+                <div><a className="link link-hover">Forgot password?</a></div>
+                <input className='btn btn-neutral' type="submit" value='Login' />
+                
+              </form>
+            </div>
+          </div>
         </div>
+      </div>
     );
 };
 
