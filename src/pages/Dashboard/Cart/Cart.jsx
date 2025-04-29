c777f907e0ba2253a6dd313626eb018a6038a8a7
arjahid <jhjahidhasanar@gmail.com>
2025-04-30 00:11:57 +0600

    added table on dashboard for cart

--- src/pages/Dashboard/Cart/Cart.jsx
+++ src/pages/Dashboard/Cart/Cart.jsx
@@ -1,18 +1,110 @@
-import React from 'react';
-import useCart from '../../../Hooks/useCart';
+import React from "react";
+import useCart from "../../../Hooks/useCart";
 
 const Cart = () => {
-     const [cart]=useCart();
-     const totalprice=cart.reduce((total,item)=>total +item.price,0)
-    return (
-        <div>
-          <div className='flex justify-evenly'>
-          <h2 className='text-4xl'>Item :{cart.length}</h2>  
-          <h2 className='text-4xl'>Tota Price :{totalprice}</h2>  
-          <button className='btn btn-primary'>Pay</button>
-          </div>
-        </div>
-    );
+  const [cart] = useCart();
+  const totalprice = cart.reduce((total, item) => total + item.price, 0);
+  return (
+    <div>
+      <div className="flex justify-evenly">
+        <h2 className="text-4xl">Item :{cart.length}</h2>
+        <h2 className="text-4xl">Tota Price :{totalprice}</h2>
+        <button className="btn btn-primary">Pay</button>
+      </div>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>
+               #
+              </th>
+              <th>image</th>
+              <th>Name</th>
+              <th>Price </th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            {
+                cart.map((item) =>  <tr key={item._id}>
+                <th>
+                  <label>
+                    <input type="checkbox" className="checkbox" />
+                  </label>
+                </th>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle h-12 w-12">
+                        <img
+                          src={item.image}
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">Hart Hagerty</div>
+                      <div className="text-sm opacity-50">United States</div>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  Zemlak, Daniel and Leannon
+                  <br />
+                  <span className="badge badge-ghost badge-sm">
+                    Desktop Support Technician
+                  </span>
+                </td>
+                <td>Purple</td>
+                <th>
+                  <button className="btn btn-ghost btn-xs">details</button>
+                </th>
+              </tr>)
+            }
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <td>
+                <div className="flex items-center gap-3">
+                  <div className="avatar">
+                    <div className="mask mask-squircle h-12 w-12">
+                      <img
+                        src="https://img.daisyui.com/images/profile/demo/2@94.webp"
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                  <div>
+                    <div className="font-bold">Hart Hagerty</div>
+                    <div className="text-sm opacity-50">United States</div>
+                  </div>
+                </div>
+              </td>
+              <td>
+                Zemlak, Daniel and Leannon
+                <br />
+                <span className="badge badge-ghost badge-sm">
+                  Desktop Support Technician
+                </span>
+              </td>
+              <td>Purple</td>
+              <th>
+                <button className="btn btn-ghost btn-xs">details</button>
+              </th>
+            </tr>
+          
+          </tbody>
+          {/* foot */}
+         
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
