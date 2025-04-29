5991258005078bc5ca1a208c59947a5c19e22a86
arjahid <jhjahidhasanar@gmail.com>
2025-04-30 00:20:23 +0600

    add handle delete function and sweet aler

--- src/pages/Dashboard/Cart/Cart.jsx
+++ src/pages/Dashboard/Cart/Cart.jsx
@@ -1,18 +1,39 @@
 import React from "react";
 import useCart from "../../../Hooks/useCart";
+import { FaTrash } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const Cart = () => {
   const [cart] = useCart();
   const totalprice = cart.reduce((total, item) => total + item.price, 0);
+  const handleDelete=(id)=>{
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+      }).then((result) => {
+        if (result.isConfirmed) {
+        //   Swal.fire({
+        //     title: "Deleted!",
+        //     text: "Your file has been deleted.",
+        //     icon: "success"
+        //   });
+        }
+      });
+  }
   return (
     <div>
-      <div className="flex justify-evenly">
+      <div className="flex justify-evenly mb-6">
         <h2 className="text-4xl">Item :{cart.length}</h2>
         <h2 className="text-4xl">Tota Price :{totalprice}</h2>
         <button className="btn btn-primary">Pay</button>
       </div>
       <div className="overflow-x-auto">
-        <table className="table">
+        <table className="table  w-full">
           {/* head */}
           <thead>
             <tr>
@@ -28,11 +49,9 @@ const Cart = () => {
           <tbody>
             {/* row 1 */}
             {
-                cart.map((item) =>  <tr key={item._id}>
+                cart.map((item,index) =>  <tr key={item._id}>
                 <th>
-                  <label>
-                    <input type="checkbox" className="checkbox" />
-                  </label>
+                 {index+1}
                 </th>
                 <td>
                   <div className="flex items-center gap-3">
@@ -44,22 +63,18 @@ const Cart = () => {
                         />
                       </div>
                     </div>
-                    <div>
-                      <div className="font-bold">Hart Hagerty</div>
-                      <div className="text-sm opacity-50">United States</div>
-                    </div>
+                   
                   </div>
                 </td>
                 <td>
-                  Zemlak, Daniel and Leannon
-                  <br />
-                  <span className="badge badge-ghost badge-sm">
-                    Desktop Support Technician
-                  </span>
+                  {item.name}
+                 
                 </td>
-                <td>Purple</td>
+                <td>${item.price}</td>
                 <th>
-                  <button className="btn btn-ghost btn-xs">details</button>
+                  <button onCanPlay={()=>handleDelete(item._id)} className="btn btn-ghost btn-lg">
+                    <FaTrash className="text-red-600"></FaTrash>
+                  </button>
                 </th>
               </tr>)
             }
