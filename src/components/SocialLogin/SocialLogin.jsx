017c0f9cef480a4c2264800a49df96155ebf26a6
arjahid <jhjahidhasanar@gmail.com>
2025-05-11 00:45:17 +0600

    add google signin

--- src/components/SocialLogin/SocialLogin.jsx
+++ src/components/SocialLogin/SocialLogin.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import { FaGoogle } from 'react-icons/fa';
+import useAuth from '../../Hooks/useAuth';
+import { useNavigate } from 'react-router-dom';
 
 const SocialLogin = () => {
-    return (
+    const {googleSignIn}=useAuth()
+    const navigate=useNavigate()
+
+    const handleGoogleSignIn = () => {
+        googleSignIn()
+            .then((result) => {
+                const loggedUser = result.user;
+                console.log(loggedUser);
+                alert("User Login Successfully");
+                navigate('/');
+                // Handle successful login here (e.g., navigate to a different page)
+            })
+            .catch((error) => {
+                console.error("Error during Google sign-in:", error);
+                // Handle error here (e.g., show an error message)
+            });
+    }
+     return (
         <div>
-            <button className='btn btn-outline btn-neutral w-full bg-green-800'>
+            <button onClick={handleGoogleSignIn} className='btn btn-outline btn-neutral w-full bg-green-800'>
                 <FaGoogle></FaGoogle>
                 Google
             </button>
