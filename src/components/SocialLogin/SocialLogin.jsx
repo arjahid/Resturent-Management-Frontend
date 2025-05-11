b6ceb0e994c8de17a417e91d399ce4277667535f
arjahid <jhjahidhasanar@gmail.com>
2025-05-11 17:59:27 +0600

    added existing user functionality

--- src/components/SocialLogin/SocialLogin.jsx
+++ src/components/SocialLogin/SocialLogin.jsx
@@ -2,18 +2,30 @@ import React from 'react';
 import { FaGoogle } from 'react-icons/fa';
 import useAuth from '../../Hooks/useAuth';
 import { useNavigate } from 'react-router-dom';
+import useAxiousPublic from '../../Hooks/useAxiousPublic';
 
 const SocialLogin = () => {
     const {googleSignIn}=useAuth()
     const navigate=useNavigate()
+
+    const axiousPublic=useAxiousPublic()
 
     const handleGoogleSignIn = () => {
         googleSignIn()
             .then((result) => {
                 const loggedUser = result.user;
                 console.log(loggedUser);
-                alert("User Login Successfully");
+                const userInfo={
+                    email:result.user?.email,
+                    name:result.user?.displayName,
+                }
+                axiousPublic.post('/users',userInfo)
+                .then(res=>{
+                    console.log(res.data);
+                    alert("User Login Successfully");
                 navigate('/');
+                })
+                
                 // Handle successful login here (e.g., navigate to a different page)
             })
             .catch((error) => {
