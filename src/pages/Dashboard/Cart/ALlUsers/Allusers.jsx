d5a3af04b0f9d1e1741b5e134cc7ad998a87eee9
arjahid <jhjahidhasanar@gmail.com>
2025-05-12 22:34:47 +0600

    get users data using tanstack query

--- src/pages/Dashboard/Cart/ALlUsers/Allusers.jsx
+++ src/pages/Dashboard/Cart/ALlUsers/Allusers.jsx
@@ -1,11 +1,22 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import useAxious from '../../../../Hooks/useAxious';
 
 const Allusers = () => {
+    const axiousSecure=useAxious();
+    const {data:users=[]}=useQuery({
+        queryKey:['users'],
+        queryFn: async()=>{
+            const res=await axiousSecure.get('/users');
+            return res.data;
+
+        }
+    })
     return (
         <div>
            <div className='flex justify-evenly my-4'>
             <h2 className='text-3xl'>All users</h2>
-            <h2 className='text-3xl'>Total users</h2>
+            <h2 className='text-3xl'>Total users: {users.length}</h2>
            </div>
         </div>
     );
