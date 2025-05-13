683c05b86a9b7adfcd25c7a69f1189e529e98ed6
arjahid <jhjahidhasanar@gmail.com>
2025-05-14 00:19:30 +0600

    add axiousSecure functionality for jwt token

--- src/Hooks/useAxious.jsx
+++ src/Hooks/useAxious.jsx
@@ -5,6 +5,17 @@ const axiousSecure = axios.create({
 });
 
 const useAxious = () => {
+  axiousSecure.interceptors.request.use((config)=>{
+    const token=localStorage.getItem("access-token");
+    config.headers.authorization=`Bearer ${token}`;
+    // You can log the request config or token here for debugging
+    console.log("Request Config:",token);
+    return config;
+  },
+  (err)=>{
+    return Promise.reject(err);
+  }
+)
   return axiousSecure; // Directly return the Axios instance
 };
 
