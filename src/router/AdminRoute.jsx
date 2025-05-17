beced1c36289d5aac0a18973092dcd27fd0764f3
arjahid <jhjahidhasanar@gmail.com>
2025-05-17 23:56:08 +0600

    create admin route

--- src/router/AdminRoute.jsx
+++ src/router/AdminRoute.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import useAuth from '../Hooks/useAuth';
 import { useLocation } from 'react-router-dom';
+import useAdmin from '../Hooks/useAdmin';
 
 const AdminRoute = (children) => {
    const {user,loading}=useAuth();
+   const [isAdmin,isisAdminLoading]=useAdmin()
+   const location=useLocation()
 
-   if(loading){
-    return <div className="flex justify-center items-center h-screen">
-        <progress className="progress w-56"></progress>
-    </div>
-   }
+    if(loading || isisAdminLoading){
+        return <div className="flex justify-center items-center h-screen">
+            <progress className="progress w-56"></progress>
+        </div>
+    }
+    if(user){
+        return children;
+    }
+    return <Navigate to='/login' state={{from:location}}></Navigate>
+   
 };
 
 export default AdminRoute;
