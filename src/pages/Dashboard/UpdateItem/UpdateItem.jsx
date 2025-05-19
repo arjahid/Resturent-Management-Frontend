1b972061de24bf1e4fe796e804d6bf8dc8f61bd7
arjahid <jhjahidhasanar@gmail.com>
2025-05-19 14:05:08 +0600

    fix updateElement datbase link

--- src/pages/Dashboard/UpdateItem/UpdateItem.jsx
+++ src/pages/Dashboard/UpdateItem/UpdateItem.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import { useLoaderData } from 'react-router-dom';
 
 const UpdateItem = () => {
+    const data=useLoaderData();
+    console.log('le ',data);
     return (
         <div>
             <SectionTitle heading="Update an Item" subHeading="Refresh Info"></SectionTitle>
